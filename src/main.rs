6774b397838ffee1b08efb0fee98224c48650e93
saiumesh-apty <saiumesh.dhanewar@apty.io>
2020-02-06 18:12:36 +0530

    added login handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer };
+
+// routes
+mod routes;
+use routes::auth::auth_routes;
+
+// types
+mod types;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(auth_routes())
+    }).bind("127.0.0.1:8000")?.run().await
 }
